7f75c10ed889e37321ff5258e9faa70aa4dbe707
Sidharth Kapoor <sidharthkapoor085@gmail.com>
2023-12-23 07:37:27 +0530

    Added Category Buttons

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -2,6 +2,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,7 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.mvvm.compose.getViewModel
@@ -36,10 +40,26 @@ import model.BirdImage
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
+@Composable
+fun BirdAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme (
+        colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            AbsoluteCutCornerShape(0.dp),
+            AbsoluteCutCornerShape(0.dp),
+            AbsoluteCutCornerShape(0.dp)
+        )
+    ) {
+        content()
+    }
+}
+
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    MaterialTheme {
+    BirdAppTheme {
         val birdsViewModel: BirdsViewModel = getViewModel(Unit, viewModelFactory {
             BirdsViewModel()
         })
@@ -50,19 +70,48 @@ fun App() {
 @Composable
 fun BirdsPage(viewModel: BirdsViewModel) {
     val uiState: BirdsUiState by viewModel.uiState.collectAsState()
-    AnimatedVisibility(uiState.images.isNotEmpty()) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(5.dp),
-            verticalArrangement = Arrangement.spacedBy(5.dp),
-            modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp),
-            content = {
-                items(uiState.images) {
-                    BirdsImageCell(it)
+
+    Column (
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row (
+            Modifier.fillMaxWidth().padding(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            for (category in uiState.categories) {
+                Button(
+                    onClick = {
+                        viewModel.selectCategory(category)
+                    }, modifier = Modifier
+                        .aspectRatio(1.0f)
+                        .fillMaxSize()
+                        .weight(1.0f),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        focusedElevation = 0.dp
+                    )
+                ) {
+                    Text(category)
                 }
             }
+        }
 
-        )
+        AnimatedVisibility(uiState.selectedImages.isNotEmpty()) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp),
+                content = {
+                    items(uiState.selectedImages) {
+                        BirdsImageCell(it)
+                    }
+                }
+
+            )
+        }
     }
 }
 
--- shared/src/commonMain/kotlin/BirdsViewModel.kt
+++ shared/src/commonMain/kotlin/BirdsViewModel.kt
@@ -11,8 +11,17 @@ import kotlinx.coroutines.launch
 import model.BirdImage
 
 data class BirdsUiState(
-    val images : List<BirdImage> = emptyList()
-)
+    val images : List<BirdImage> = emptyList(),
+    val selectedCategory : String ?= null
+) {
+    val categories = images.map {
+        it.category
+    }.toSet()
+
+    val selectedImages = images.filter {
+        it.category == selectedCategory
+    }
+}
 
 class BirdsViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(BirdsUiState())
@@ -30,6 +39,12 @@ class BirdsViewModel: ViewModel() {
 
     override fun onCleared() {
         httpClient.close()
+    }
+
+    fun selectCategory(category: String) {
+        _uiState.update {
+            it.copy(selectedCategory = category)
+        }
     }
 
     fun updateImages() {
